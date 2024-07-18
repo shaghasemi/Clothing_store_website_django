252dde59c11d797b3a37340f71c5db26e6dce1f2
Hamed Shahghasemi <shaghasemi@gmail.com>
2024-07-18 12:39:20 +0330

    build

--- clothingShopProject/settings.py
+++ clothingShopProject/settings.py
@@ -32,7 +32,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -175,3 +174,4 @@ LOGIN_URL = '/user/login/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
